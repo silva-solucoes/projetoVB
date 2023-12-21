2a703023dae86a660986bd4fe467a55add38a19d
Silva Soluções Tech <123086804+silva-solucoes@users.noreply.github.com>
2023-12-21 12:19:53 -0300

    Adicionar arquivos de projeto.

---
+++ Form1.Designer.vb
@@ -0,0 +1,63 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        GroupBox1 = New GroupBox()
+        GroupBox2 = New GroupBox()
+        SuspendLayout()
+        ' 
+        ' GroupBox1
+        ' 
+        GroupBox1.Location = New Point(3, 420)
+        GroupBox1.Name = "GroupBox1"
+        GroupBox1.Size = New Size(397, 424)
+        GroupBox1.TabIndex = 0
+        GroupBox1.TabStop = False
+        ' 
+        ' GroupBox2
+        ' 
+        GroupBox2.Location = New Point(400, 420)
+        GroupBox2.Name = "GroupBox2"
+        GroupBox2.Size = New Size(397, 424)
+        GroupBox2.TabIndex = 1
+        GroupBox2.TabStop = False
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(11.0F, 28.0F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(799, 851)
+        Controls.Add(GroupBox2)
+        Controls.Add(GroupBox1)
+        Font = New Font("Segoe UI", 12.0F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        Margin = New Padding(4)
+        Name = "Form1"
+        StartPosition = FormStartPosition.CenterScreen
+        Text = "Área de Acesso | SADILajes"
+        ResumeLayout(False)
+    End Sub
+
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents GroupBox2 As GroupBox
+
+End Class
